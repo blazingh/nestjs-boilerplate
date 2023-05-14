92ead6a19b8fab1ac8ae671b21db42388a635e25
blazingh <hadibaalbaki2@gmail.com>
2023-05-14 13:13:01 +0300

    formated file

--- src/components/auth/auth.controller.ts
+++ src/components/auth/auth.controller.ts
@@ -9,7 +9,7 @@ import {
   UnauthorizedException,
   UseGuards,
   NotFoundException,
-} from '@nestjs/common'
+} from '@nestjs/common';
 import {
   ApiTags,
   ApiBody,
@@ -18,30 +18,30 @@ import {
   ApiUnauthorizedResponse,
   ApiBearerAuth,
   ApiNotFoundResponse,
-} from '@nestjs/swagger'
-import { JwtService } from '@nestjs/jwt'
+} from '@nestjs/swagger';
+import { JwtService } from '@nestjs/jwt';
 
-import UsersService from '@components/users/users.service'
-import UserEntity from '@components/users/entities/user.entity'
-import JwtAuthGuard from '@guards/jwtAuth.guard'
+import UsersService from '@components/users/users.service';
+import UserEntity from '@components/users/entities/user.entity';
+import JwtAuthGuard from '@guards/jwtAuth.guard';
 
-import OkResponseDto from '@dto/okResponse.dto'
-import { IAuthLoginOutput } from './interfaces/IAuthLoginOutput.interface'
-import LocalAuthGuard from './guards/localAuth.guard'
-import AuthService from './auth.service'
-import RefreshTokenDto from './dto/refreshToken.dto'
-import SignInDto from './dto/signIn.dto'
-import SignUpDto from './dto/signUp.dto'
-import VerifyUserDto from './dto/verifyUser.dto'
+import OkResponseDto from '@dto/okResponse.dto';
+import { IAuthLoginOutput } from './interfaces/IAuthLoginOutput.interface';
+import LocalAuthGuard from './guards/localAuth.guard';
+import AuthService from './auth.service';
+import RefreshTokenDto from './dto/refreshToken.dto';
+import SignInDto from './dto/signIn.dto';
+import SignUpDto from './dto/signUp.dto';
+import VerifyUserDto from './dto/verifyUser.dto';
 
 @ApiTags('Auth')
 @Controller('auth')
 export default class AuthController {
-  constructor (
+  constructor(
     private readonly authService: AuthService,
     private readonly jwtService: JwtService,
     private readonly usersService: UsersService,
-  ) {}
+  ) { }
 
   @ApiBody({ type: SignInDto })
   @ApiOkResponse({ description: 'Returns jwt tokens' })
@@ -50,20 +50,20 @@ export default class AuthController {
   @HttpCode(200)
   @UseGuards(LocalAuthGuard)
   @Post('sign-in')
-  async signIn (@Request() req) {
-    return this.authService.login(req.user)
+  async signIn(@Request() req) {
+    return this.authService.login(req.user);
   }
 
   @ApiBody({ type: SignUpDto })
   @ApiOkResponse({ description: '200, Success' })
   @ApiInternalServerErrorResponse({ description: '500. InternalServerError' })
   @Post('sign-up')
-  async signUp (@Body() user: SignUpDto): Promise<OkResponseDto> {
-    await this.usersService.create(user)
+  async signUp(@Body() user: SignUpDto): Promise<OkResponseDto> {
+    await this.usersService.create(user);
 
     return {
       message: 'The item was created successfully',
-    }
+    };
   }
 
   @ApiOkResponse({ description: '200, returns new jwt tokens' })
@@ -71,30 +71,29 @@ export default class AuthController {
   @ApiInternalServerErrorResponse({ description: '500. InternalServerError ' })
   @ApiBearerAuth()
   @Post('refreshToken')
-  async refreshToken (
+  async refreshToken(
     @Body() refreshTokenDto: RefreshTokenDto,
   ): Promise<IAuthLoginOutput | never> {
-    const verifiedUser = this.jwtService.verify(refreshTokenDto.refreshToken)
+    const verifiedUser = this.jwtService.verify(refreshTokenDto.refreshToken);
 
-    const oldRefreshToken: string = await this.authService.getRefreshTokenByEmail(
-      verifiedUser.email,
-    )
+    const oldRefreshToken: string =
+      await this.authService.getRefreshTokenByEmail(verifiedUser.email);
 
     // if the old refresh token is not equal to request refresh token then this user is unauthorized
     if (!oldRefreshToken || oldRefreshToken !== refreshTokenDto.refreshToken) {
       throw new UnauthorizedException(
         'Authentication credentials were missing or incorrect',
-      )
+      );
     }
 
     const payload = {
       id: verifiedUser.id,
       email: verifiedUser.email,
-    }
+    };
 
-    const newTokens = await this.authService.login(payload)
+    const newTokens = await this.authService.login(payload);
 
-    return newTokens
+    return newTokens;
   }
 
   @ApiOkResponse({
@@ -105,21 +104,21 @@ export default class AuthController {
     description: 'User was not found',
   })
   @Post('verify')
-  async verifyUser (
+  async verifyUser(
     @Body() verifyUserDto: VerifyUserDto,
   ): Promise<boolean | never> {
     const foundUser: UserEntity = await this.usersService.getByEmail(
       verifyUserDto.email,
       false,
-    )
+    );
 
     if (!foundUser) {
-      throw new NotFoundException('The user does not exist')
+      throw new NotFoundException('The user does not exist');
     }
 
-    await this.usersService.update(foundUser._id, { verified: true })
+    await this.usersService.update(foundUser._id, { verified: true });
 
-    return true
+    return true;
   }
 
   @ApiOkResponse({ description: '200, returns new jwt tokens' })
@@ -129,13 +128,13 @@ export default class AuthController {
   @UseGuards(JwtAuthGuard)
   @Delete('logout/:token')
   @HttpCode(204)
-  async logout (@Param('token') token: string): Promise<void | never> {
-    const { email } = this.jwtService.verify(token)
+  async logout(@Param('token') token: string): Promise<void | never> {
+    const { email } = this.jwtService.verify(token);
 
-    const deletedUserCount = await this.authService.deleteTokenByEmail(email)
+    const deletedUserCount = await this.authService.deleteTokenByEmail(email);
 
     if (deletedUserCount === 0) {
-      throw new NotFoundException('The item does not exist')
+      throw new NotFoundException('The item does not exist');
     }
   }
 
@@ -145,7 +144,7 @@ export default class AuthController {
   @UseGuards(JwtAuthGuard)
   @Delete('logoutAll')
   @HttpCode(204)
-  async logoutAll (): Promise<void> {
-    await this.authService.deleteAllTokens()
+  async logoutAll(): Promise<void> {
+    await this.authService.deleteAllTokens();
   }
 }
